cd2e25efb8e73a15f9f6c662f7f590fde2c03299
Julia  Kulbaczewska <juliakulbaczewska@macbook-pro-julia.home>
2020-03-27 14:14:01 +0100

    burger menu added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-
-$(document).ready(function(){
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+$(document).ready(function () {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
         $(this).toggleClass('open');
     });
 });
+
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
